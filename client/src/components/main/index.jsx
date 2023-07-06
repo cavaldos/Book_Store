c7d01f933e76fc3bc788b771989c09b0f864ee8a
capucins <khanhnguyen297kn@gmail.com>
2023-07-06 09:45:27 +0700

    a

--- client/src/components/main/index.jsx
+++ client/src/components/main/index.jsx
@@ -1,107 +1,106 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import HomeIcon from '@material-ui/icons/Home';
 import BookIcon from '@material-ui/icons/Book';
 import { Link } from 'react-router-dom';
 
-const drawerWidth = 240;
-
 const useStyles = makeStyles((theme) => ({
     root: {
-        display: 'flex',
-    },
-    appBar: {
-        zIndex: 1300,
+        flexGrow: 1,
     },
     menuButton: {
         marginRight: theme.spacing(2),
     },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
+    header: {
+        backgroundColor: '#fff',
+        height: '64px',
+        width: '100%',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.1)',
+        zIndex: 1000,
     },
-    drawerPaper: {
-        width: drawerWidth,
+    sidebar: {
+        backgroundColor: '#f0f0f0',
+        height: '100vh',
+        minWidth: '200px',
+        padding: theme.spacing(2),
+        boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.1)',
+        position: 'fixed',
+        top: '64px',
+        left: 0,
     },
     content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
+        marginLeft: '200px',
+        padding: theme.spacing(2),
     },
-    toolbar: theme.mixins.toolbar,
 }));
 
 const DefaultLayout = ({ children }) => {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
+    const handleSidebarToggle = () => {
+        setOpen(!open);
     };
 
     return (
         <div className={classes.root}>
-            <AppBar position="fixed" className={classes.appBar}>
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Link
-                        to="/"
-                        style={{ textDecoration: 'none', color: 'inherit' }}
-                    >
-                        <h1>Book Store</h1>
-                    </Link>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                className={classes.drawer}
-                variant="temporary"
-                anchor="left"
-                open={open}
-                onClose={handleDrawerClose}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.toolbar} />
-                <List>
-                    <ListItem button component={Link} to="/">
-                        <ListItemIcon>
-                            <HomeIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Home" />
-                    </ListItem>
-                    <ListItem button component={Link} to="/books">
-                        <ListItemIcon>
-                            <BookIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Books" />
-                    </ListItem>
-                </List>
-            </Drawer>
-            <main className={classes.content}>
-                <div className={classes.toolbar} />
-                {children}
-            </main>
+            <div className={classes.header}>
+                <IconButton
+                    className={classes.menuButton}
+                    color="inherit"
+                    aria-label="open sidebar"
+                    onClick={handleSidebarToggle}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Link
+                    to="/"
+                    style={{ textDecoration: 'none', color: 'inherit' }}
+                >
+                    <h1>Book Store</h1>
+                </Link>
+            </div>
+            <Grid container>
+                <Grid
+                    item
+                    className={classes.sidebar}
+                    xs={12}
+                    sm={3}
+                    md={2}
+                    lg={2}
+                >
+                    <ul>
+                        <li>
+                            <Link to="/">
+                                <HomeIcon />
+                                <span>Home</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/books">
+                                <BookIcon />
+                                <span>Books</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </Grid>
+                <Grid
+                    item
+                    className={classes.content}
+                    xs={12}
+                    sm={9}
+                    md={10}
+                    lg={10}
+                >
+                    {children}
+                </Grid>
+            </Grid>
         </div>
     );
 };
