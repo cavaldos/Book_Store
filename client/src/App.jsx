7c7239581e33882a51daa73332e927821ade2d74
capucins <khanhnguyen297kn@gmail.com>
2023-07-09 22:55:47 +0700

    khanh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { publicRoutes, privateRoutes } from './Routes';
-import { publicRoutes } from './Routes';
-import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 
 import './App.scss';
 import DefaultLayout from './components/main';
@@ -35,31 +33,6 @@ const App = () => {
                     {/*  */}
                 </Routes>
             </div>
-            <ThemeProvider>
-                <CSSReset />
-                {/* <div className="App"> */}
-                <Routes>
-                    {/*  */}
-                    {publicRoutes.map((route, index) => {
-                        const Layout =
-                            route.layout === null ? Fragment : DefaultLayout;
-                        const Page = route.component;
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                    {/*  */}
-                </Routes>
-                {/* </div> */}
-            </ThemeProvider>
         </Router>
     );
 };
