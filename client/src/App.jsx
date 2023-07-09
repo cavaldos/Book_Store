186983644b68b7e3a94ce7dd6c22279242747201
capucins <khanhnguyen297kn@gmail.com>
2023-07-09 22:42:36 +0700

    khanh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
 import { publicRoutes, privateRoutes } from './Routes';
-=======
 import { publicRoutes } from './Routes';
-import { CSSReset, ThemeProvider } from "@chakra-ui/core";
->>>>>>> 379086920a1b6c1332b496996476c82cc9503e7c
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 
 import './App.scss';
 import DefaultLayout from './components/main';
@@ -16,7 +13,6 @@ const App = () => {
 
     return (
         <Router>
-<<<<<<< HEAD
             <div className="App">
                 <Routes>
                     {/*  */}
@@ -39,33 +35,31 @@ const App = () => {
                     {/*  */}
                 </Routes>
             </div>
-=======
             <ThemeProvider>
                 <CSSReset />
                 {/* <div className="App"> */}
-                    <Routes>
-                        {/*  */}
-                        {publicRoutes.map((route, index) => {
-                            const Layout =
-                                route.layout === null ? Fragment : DefaultLayout;
-                            const Page = route.component;
-                            return (
-                                <Route
-                                    key={index}
-                                    path={route.path}
-                                    element={
-                                        <Layout>
-                                            <Page />
-                                        </Layout>
-                                    }
-                                />
-                            );
-                        })}
-                        {/*  */}
-                    </Routes>
+                <Routes>
+                    {/*  */}
+                    {publicRoutes.map((route, index) => {
+                        const Layout =
+                            route.layout === null ? Fragment : DefaultLayout;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                    {/*  */}
+                </Routes>
                 {/* </div> */}
             </ThemeProvider>
->>>>>>> 379086920a1b6c1332b496996476c82cc9503e7c
         </Router>
     );
 };
