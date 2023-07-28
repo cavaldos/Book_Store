8d5b4151834151fc263b110010fabadae39ed798
bourbon <96412994+cavaldos@users.noreply.github.com>
2023-07-28 14:20:58 +0700

    Delete client directory

--- client/config-overrides.js
+++
@@ -1,6 +0,0 @@
-const { override, useBabelRc } = require("customize-cra");
-const { addReactRefresh } = require("customize-cra-react-refresh");
-
-/* config-overrides.js */
-module.exports = override(useBabelRc(), addReactRefresh());
- 
