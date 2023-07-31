a1bb2b32172aff0b042f48a7add2b8e78e85abdd
capucins <khanhnguyen297kn@gmail.com>
2023-07-31 19:33:43 +0700

    .

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,16 +5,14 @@ import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  () => {
-    reportWebVitals();
-  }
+  </React.StrictMode>
 );
+reportWebVitals();
--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,26 +1,33 @@
-// import { createStore } from 'redux'
-// import combineReducers from "redux";
-import { configureStore } from '@reduxjs/toolkit';
-import bookSlice from './features/bookSlice';
-import userSlice from './features/userSilce';
-import roleSlice from './features/roleSlice';
-import themeSlice from './features/themeSlice';
-import paymentSlice from './features/paymentSlice';
-// import roleSlice  from './features/roleSlice';
-const store = configureStore(
-    {
-        reducer: {
-            book: bookSlice,
-            user: userSlice,
-            role: roleSlice,
-            theme: themeSlice,
-            payment: paymentSlice,
-         
-        },
-    },
-    {},
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+import bookSlice from "./features/bookSlice";
+import userSlice from "./features/userSilce";
+import roleSlice from "./features/roleSlice";
+import themeSlice from "./features/themeSlice";
+import paymentSlice from "./features/paymentSlice";
+
+
+
+import combineReducers from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const store = configureStore({
+  reducer: {
+    book: bookSlice,
+    user: userSlice,
+    role: roleSlice,
+    theme: themeSlice,
+    payment: paymentSlice,
+  },
+});
 
 export default store;
