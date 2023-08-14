3f822b2e9a4220627246dcae3c1c8c875240e034
hung3009 <nguyen.vu.hung309@gmail.com>
2023-08-14 22:59:40 +0700

    carousel slider2

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -5,8 +5,6 @@ import Book from "./book/book";
 import { Spin, Pagination } from "antd";
 import { Select } from "antd";
 import Fillter from "./fillter/fillter";
-//import Product from "../User/Cart/Product";
-// import { Carousel } from "antd";
 import { Col, Row } from "antd";
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
