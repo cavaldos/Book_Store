ca7a0d8c8126e52511d821e845ce5c434123ca86
hung3009 <nguyen.vu.hung309@gmail.com>
2023-08-15 17:21:55 +0700

    add payment server

---
+++ paymentsv/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+
+
+const PORT = 4000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.use(express.json());
+dotenv.config();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+//ROUTES
+
+
+const payment_server = app.listen(PORT, () => {
+    console.log(`Server payment is running on port : ${PORT}`);
+});
+
+
+
