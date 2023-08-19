3e502dfe4aeaa3b18ccd6bd7084f1bb81433ce94
chowsonice <56868942+chowsonice@users.noreply.github.com>
2023-08-19 09:00:21 -0700

    (To be implemented) Reconfirm before updating book quantity

---
+++ client/src/pages/Admin/option/importBook.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
+const ImportBooks = (stock) => {
+  // http://localhost:8001/deleteuser
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // console.log("record",email);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        See details
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
+};
+export default ImportBooks;
