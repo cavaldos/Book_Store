3ff291287cffda4abaf20fd2a80e9933a0b9c639
chowsonice <56868942+chowsonice@users.noreply.github.com>
2023-08-19 05:33:17 -0700

    Config of cloudinary service

---
+++ server/config/cloudinary.config.js
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'didfj5vwp', 
+  api_key: cloudinary_clientID, 
+  api_secret: cloudinary_clientSecret
+});
